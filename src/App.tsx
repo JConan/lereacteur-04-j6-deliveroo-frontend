8d95c8c2c52394464f30303b496fccd7155b78be
jconan <conanjahh@hotmail.com>
2021-07-22 01:45:47 +0200

    added a spinner at initialization

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header, { useHeaderState } from "./components/Header";
 import MenuCategories, {
   useMenuCategoriesState,
 } from "./components/MenuCategories";
+import Spinner from "./components/Spinner";
 
 function App() {
   const backendApiResponse = useBackendApi();
@@ -13,30 +14,38 @@ function App() {
   const menuCategories = useMenuCategoriesState(backendApiResponse);
   const cartState = useCartState();
 
+  const spin = true;
+
   return (
     <div className="App">
-      {header && <Header {...header} />}
-      <div className="content">
-        <div className="menu">
-          {menuCategories?.map((menuCategories, index) => (
-            <MenuCategories
-              key={index}
-              {...menuCategories}
-              onItemClick={cartState.dispatch}
-            />
-          ))}
-        </div>
-        <Cart {...cartState} />
-      </div>
-      <div className="footer">
-        <div>
-          Made by Johan CHAN with ReactJs
-          <a href="https://www.lereacteur.io/">@LeReacteur</a>
-          <a href="https://github.com/JConan/lereacteur-exo-react_deliveroo-frontend">
-            Github
-          </a>
-        </div>
-      </div>
+      {backendApiResponse && header ? (
+        <>
+          <Header {...header!} />
+          <div className="content">
+            <div className="menu">
+              {menuCategories?.map((menuCategories, index) => (
+                <MenuCategories
+                  key={index}
+                  {...menuCategories}
+                  onItemClick={cartState.dispatch}
+                />
+              ))}
+            </div>
+            <Cart {...cartState} />
+          </div>
+          <div className="footer">
+            <div>
+              Made by Johan CHAN with ReactJs
+              <a href="https://www.lereacteur.io/">@LeReacteur</a>
+              <a href="https://github.com/JConan/lereacteur-exo-react_deliveroo-frontend">
+                Github
+              </a>
+            </div>
+          </div>
+        </>
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 }
